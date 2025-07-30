b392ea954bd40cf3080e3a5c8c09231e31d2a38e
k ostaN <pyotrthevampire@gmail.com>
2025-07-30 00:18:28 -0500

    Create 03_extract_files.sh

---
+++ setup/03_extract_files.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+source ./setup/shared.sh
+load_config
+check_root
+
+log_title "Step 3: Extracting Base System"
+
+cd "$BUILD_DIR"
+
+log_step "Extracting bootfs..."
+tar xjfp bootfs_20250128.tar.bz2 -C bootfs
+
+log_step "Extracting stage3 to rootfs..."
+tar xfp stage3-arm64-openrc-splitusr-*.tar.xz -C rootfs
+
+log_step "Extracting kernel modules..."
+mkdir -p rootfs/lib/modules
+tar xpjf rootfs_modules_20250128.tar.bz2 -C rootfs/lib/modules
+
+log_step "Extracting firmware..."
+mkdir -p rootfs/lib/firmware
+tar xpjf rootfs_firmware-nonfree_20250128.tar.bz2 -C rootfs/lib/firmware
+tar xpjf rootfs_firmware-bluez_20250128.tar.bz2 -C rootfs/lib/firmware
+
+log_step "Extracting kernel sources..."
+mkdir -p rootfs/usr/src/linux-6.6.74-raspberrypi_20250128
+tar xpjf linux-6.6.74-raspberrypi_20250128.tar.bz2 -C rootfs/usr/src/linux-6.6.74-raspberrypi_20250128
+
+log_step "Creating kernel symlink..."
+cd rootfs/usr/src
+ln -s linux-6.6.74-raspberrypi_20250128 linux
+cd ../../..
+
+log_success "All base files extracted"
