138f80a420c2d0ca20c5f45a1ebc97f62cf81cba
k ostaN <pyotrthevampire@gmail.com>
2025-07-31 13:27:39 -0500

    Update install.sh
    When will I learn to STOP using relative paths like that

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 set -e
 
 # Load config and shared functions
-source ./config.env
-source ./setup/shared.sh
+source $BUILD_DIR/config.env
+source $BUILD_DIR/setup/shared.sh
 
 # Main script steps
 log_title "Starting Gentoo Install Script for Raspberry Pi (5)"
 
-./setup/01_downloads.sh
-./setup/02_partition_format.sh
-./setup/03_extract_files.sh
-./setup/04_config_system.sh
-./setup/05_finalize.sh
+$BUILD_DIR/setup/01_downloads.sh
+$BUILD_DIR/setup/02_partition_format.sh
+$BUILD_DIR/setup/03_extract_files.sh
+$BUILD_DIR/setup/04_config_system.sh
+$BUILD_DIR/setup/05_finalize.sh
 
 log_success "Gentoo installation complete. You may now boot your Raspberry Pi."
