43636f809c4bca69f0e1fd81e3a5bd15d5708a13
k ostaN <pyotrthevampire@gmail.com>
2025-07-31 13:44:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
+# Set directory
+DIR="/root/RASPBERRY_PI_GENTOO"
+
 # Load config and shared functions
-source $DIR/config.env
-source $DIR/setup/shared.sh
+source "$DIR/config.env"
+source "$DIR/setup/shared.sh"
 
 # Main script steps
 log_title "Starting Gentoo Install Script for Raspberry Pi (5)"
 
-$DIR/setup/01_downloads.sh
-$DIR/setup/02_partition_format.sh
-$DIR/setup/03_extract_files.sh
-$DIR/setup/04_config_system.sh
-$DIR/setup/05_finalize.sh
+"$DIR/setup/01_downloads.sh"
+"$DIR/setup/02_partition_format.sh"
+"$DIR/setup/03_extract_files.sh"
+"$DIR/setup/04_config_system.sh"
+"$DIR/setup/05_finalize.sh"
 
 log_success "Gentoo installation complete. You may now boot your Raspberry Pi."
