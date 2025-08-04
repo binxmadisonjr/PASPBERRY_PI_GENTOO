50919920936766010e45240804fa5e53cb4bc97b
k ostaN <pyotrthevampire@gmail.com>
2025-08-04 10:29:50 -0500

    Update shared.sh

--- setup/shared.sh
+++ setup/shared.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Only load once
+[[ -n "$__SHARED_SH_INCLUDED" ]] && return
+__SHARED_SH_INCLUDED=1
+
 log_title() {
   echo -e "\n\033[1;34m==> $1\033[0m"
 }
@@ -17,6 +21,10 @@ log_error() {
   exit 1
 }
 
+log_info() {
+  echo -e "\033[1;36m$1\033[0m"
+}
+
 check_root() {
   if [[ $EUID -ne 0 ]]; then
     log_error "Please run as root or with sudo"
@@ -27,7 +35,7 @@ require_vars() {
   local vars=("$@")
   for var in "${vars[@]}"; do
     if [[ -z "${!var}" ]]; then
-      log_error "Missing required config value: $var"
+      log_error "Missing required config value: $var (current value: '${!var}')"
     fi
   done
 }
